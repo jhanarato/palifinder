c1f99d61bbd2eae7bd2438f7ae7d763bdd0154d7
jr <jhanarato@gmail.com>
2026-08-07 10:20:03 +0800

    Implement looking up a stem.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
-fn main() {
-    println!("Hello, world!");
+use rusqlite::{Connection, Result};
+
+#[derive(Debug)]
+struct Stem {
+    stem: String,
+}
+
+fn main() -> Result<()> {
+    let conn = Connection::open("data/dpd.db")?;
+    let mut stmt = conn.prepare("SELECT stem from dpd_headwords where lemma_1 == 'bhagavā'")?;
+    let stems = stmt.query_map([], |row| {
+       Ok(
+           Stem {
+               stem: row.get(0)?,
+           }
+       )
+    })?;
+
+    for stem in stems {
+        println!("Found stem {:?}", stem?.stem);
+    }
+    Ok(())
 }
