0601d314e458c3d8be73f68053779e77dc4e5c4c
jr <jhanarato@gmail.com>
2026-08-08 10:47:45 +0800

    Add get_stem() function

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,40 @@ struct Stem {
     stem: String,
 }
 
+#[allow(dead_code)]
+fn get_stem(conn: &mut Connection, word: &str) -> Result<String> {
+    let stem = conn.query_row(
+        "SELECT stem from dpd_headwords where lemma_1 == (?1)",
+        [word],
+        |row| Ok(Stem { stem: row.get(0)? })
+    )?;
+    Ok(stem.stem)
+}
+
 fn main() -> Result<()> {
     let conn = Connection::open("data/dpd.db")?;
     let mut stmt = conn.prepare("SELECT stem from dpd_headwords where lemma_1 == 'bhagavā'")?;
     let stems = stmt.query_map([], |row| {
-       Ok(
-           Stem {
-               stem: row.get(0)?,
-           }
-       )
+        Ok(
+            Stem {
+                stem: row.get(0)?,
+            }
+        )
     })?;
 
     for stem in stems {
         println!("Found stem {:?}", stem?.stem);
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_stem() {
+        let mut conn = Connection::open("data/dpd.db").unwrap();
+        assert_eq!(get_stem(&mut conn, "bhagavā").unwrap(), String::from("!bhagav"));
+    }
 }
