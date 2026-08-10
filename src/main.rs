9ac4d79c1d5fc71d0c979cef7ea62df5fa0b5322
jr <jhanarato@gmail.com>
2026-08-10 10:48:04 +0800

    Deserialize a sutta with serde.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use rusqlite::{Connection, Result};
+use std::collections::HashMap;
+use rusqlite::{Connection};
+use std::fs::{File};
+use std::io::BufReader;
+use anyhow::Result;
 
 #[derive(Debug)]
 struct Stem {
@@ -9,7 +13,7 @@ fn get_stem(conn: &mut Connection, word: &str) -> Result<String> {
     let stem = conn.query_one(
         "SELECT stem from dpd_headwords where lemma_1 == (?1)",
         [word],
-        |row| Ok(Stem { stem: row.get(0)? })
+        |row| Ok(Stem { stem: row.get(0)? }),
     )?;
     Ok(stem.stem)
 }
@@ -18,6 +22,15 @@ fn main() -> Result<()> {
     let mut conn = Connection::open("data/dpd.db")?;
     let stem = get_stem(&mut conn, "bhagavā")?;
     println!("Stem of bhagavā is {stem}");
+
+    let file = File::open(
+        "/opt/sc/sc-flask/sc-data/sc_bilara_data/root/pli/ms/sutta/mn/mn1_root-pli-ms.json",
+    )?;
+    let reader = BufReader::new(file);
+    let segments: HashMap<String, String> = serde_json::from_reader(reader)?;
+    for (uid, content) in segments {
+        println!("Segment {uid} is {content}");
+    }
     Ok(())
 }
 
@@ -28,6 +41,9 @@ mod tests {
     #[test]
     fn test_get_stem() {
         let mut conn = Connection::open("data/dpd.db").unwrap();
-        assert_eq!(get_stem(&mut conn, "bhagavā").unwrap(), String::from("!bhagav"));
+        assert_eq!(
+            get_stem(&mut conn, "bhagavā").unwrap(),
+            String::from("!bhagav")
+        );
     }
 }
