b1a81ca408f3f61149124afc8e917f905ea87390
jr <jhanarato@gmail.com>
2026-08-11 12:31:19 +0800

    Add get_segments()

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use anyhow::Result;
 use rusqlite::Connection;
-use std::collections::HashMap;
+use std::collections::BTreeMap;
 
 #[derive(Debug)]
 struct Stem {
@@ -16,15 +16,21 @@ fn get_stem(conn: &mut Connection, word: &str) -> Result<String> {
     Ok(stem.stem)
 }
 
+fn get_segments(content: &str) -> Result<Vec<String>> {
+    let entries: BTreeMap<String, String> = serde_json::from_str(content)?;
+    let segments: Vec<String> = entries.values().cloned().collect();
+    Ok(segments)
+}
+
 fn main() -> Result<()> {
     let mut conn = Connection::open("data/dpd.db")?;
     let stem = get_stem(&mut conn, "bhagavā")?;
     println!("Stem of bhagavā is {stem}");
 
-    let file_data = std::fs::read_to_string("/opt/sc/sc-flask/sc-data/sc_bilara_data/root/pli/ms/sutta/mn/mn1_root-pli-ms.json")?;
-    let segments: HashMap<String, String> = serde_json::from_str(file_data.as_str())?;
-    for (uid, content) in segments {
-        println!("Segment {uid} is {content}");
+    let contents = std::fs::read_to_string("/opt/sc/sc-flask/sc-data/sc_bilara_data/root/pli/ms/sutta/mn/mn1_root-pli-ms.json")?;
+
+    for segment in get_segments(contents.as_str())? {
+        println!("{segment}");
     }
     Ok(())
 }
@@ -33,6 +39,14 @@ fn main() -> Result<()> {
 mod tests {
     use super::*;
 
+    pub const MN1_SEGMENTS: &str = r#"
+    {
+        "mn1:0.1": "Majjhima Nikāya 1 ",
+        "mn1:0.2": "Mūlapariyāyasutta ",
+        "mn1:1.1": "Evaṁ me sutaṁ—"
+    }
+    "#;
+
     #[test]
     fn test_get_stem() {
         let mut conn = Connection::open("data/dpd.db").unwrap();
@@ -40,5 +54,14 @@ mod tests {
             get_stem(&mut conn, "bhagavā").unwrap(),
             String::from("!bhagav")
         );
+    }
+
+    #[test]
+    fn test_get_segments() -> Result<()> {
+        assert_eq!(
+            get_segments(MN1_SEGMENTS)?,
+            vec!("Majjhima Nikāya 1 ", "Mūlapariyāyasutta ", "Evaṁ me sutaṁ—")
+        );
+        Ok(())
     }
 }
