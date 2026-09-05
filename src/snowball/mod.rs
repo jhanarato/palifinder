1fa6cc3c968cba2ecb12b4a481907a8368396998
jr <jhanarato@gmail.com>
2026-09-05 12:56:00 +0800

    Add #[must_use]

--- src/snowball/mod.rs
+++ src/snowball/mod.rs
@@ -11,13 +11,12 @@ pub struct Stemmer {
 }
 
 impl Stemmer {
-    /// Create a new stemmer from an algorithm
+    #[must_use]
     pub fn create() -> Self {
         Stemmer { stemmer: algorithms::pali::stem }
     }
 
-    /// Stem a single word
-    /// Please note, that the input is expected to be all lowercase (if that is applicable).
+    #[must_use]
     pub fn stem<'a>(&self, input: &'a str) -> Cow<'a, str> {
         let mut env = snowball_env::SnowballEnv::create(input);
         (self.stemmer)(&mut env);
