7ce944b6f0368128fc2ad65e2faaeb9711b2209e
jr <jhanarato@gmail.com>
2026-09-10 16:20:09 +0800

    Extract dpd_stem() method.

--- src/table.rs
+++ src/table.rs
@@ -20,26 +20,18 @@ impl TermStems {
     pub fn new(vocabulary: Vocabulary, dictionary: &Dictionary) -> Self {
         let mut entries = Vec::new();
         for term in vocabulary {
-            let stems = dictionary.stems(term.as_str());
-            match stems {
-                Err(_) => entries.push(TermStem {
-                    term: term.clone(),
-                    dpd_stem: None,
-                }),
-                Ok(stems) => match stems.first() {
-                    Some(stem) => entries.push(TermStem {
-                        term: term.clone(),
-                        dpd_stem: Some(stem.clone()),
-                    }),
-                    None => entries.push(TermStem {
-                        term: term.clone(),
-                        dpd_stem: None,
-                    }),
-                },
-            }
+            let dpd_stem = Self::dpd_stem(term.as_str(), dictionary);
+            entries.push(TermStem { term, dpd_stem });
         }
-
         Self { entries }
+    }
+
+    fn dpd_stem(term: &str, dictionary: &Dictionary) -> Option<String> {
+        let stems = dictionary.stems(term);
+        match stems {
+            Ok(stems) => stems.first().cloned(),
+            Err(_) => None,
+        }
     }
 
     #[allow(clippy::missing_errors_doc)]
