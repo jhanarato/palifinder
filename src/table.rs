d7ae90fe857738f2ba2d1ebb0e7b8b59a36d11d5
jr <jhanarato@gmail.com>
2026-09-10 16:11:43 +0800

    Refactor TermStems to use Vec<TermStem> internally.

--- src/table.rs
+++ src/table.rs
@@ -3,7 +3,6 @@ use crate::vocabulary::Vocabulary;
 use anyhow::Result;
 use csv::Writer;
 use serde::{Deserialize, Serialize};
-use std::collections::BTreeMap;
 use std::path::Path;
 
 #[derive(Clone, Debug, PartialOrd, PartialEq, Serialize, Deserialize)]
@@ -14,21 +13,30 @@ pub struct TermStem {
 
 #[derive(Clone)]
 pub struct TermStems {
-    entries: BTreeMap<String, Option<String>>,
+    entries: Vec<TermStem>,
 }
 
 impl TermStems {
     pub fn new(vocabulary: Vocabulary, dictionary: &Dictionary) -> Self {
-        let mut entries: BTreeMap<String, Option<String>> = BTreeMap::new();
+        let mut entries = Vec::new();
         for term in vocabulary {
             let stems = dictionary.stems(term.as_str());
             match stems {
-                Err(_) => entries.insert(term.clone(), None),
+                Err(_) => entries.push(TermStem {
+                    term: term.clone(),
+                    dpd_stem: None,
+                }),
                 Ok(stems) => match stems.first() {
-                    Some(stem) => entries.insert(term.clone(), Some(stem.clone())),
-                    None => entries.insert(term.clone(), None),
+                    Some(stem) => entries.push(TermStem {
+                        term: term.clone(),
+                        dpd_stem: Some(stem.clone()),
+                    }),
+                    None => entries.push(TermStem {
+                        term: term.clone(),
+                        dpd_stem: None,
+                    }),
                 },
-            };
+            }
         }
 
         Self { entries }
@@ -37,42 +45,9 @@ impl TermStems {
     #[allow(clippy::missing_errors_doc)]
     pub fn save(&self, path: &Path) -> Result<()> {
         let mut writer = Writer::from_path(path)?;
-        for (key, value) in &self.entries {
-            let term_stem = TermStem {
-                term: key.clone(),
-                dpd_stem: value.clone(),
-            };
-            writer.serialize(term_stem)?;
+        for record in &self.entries {
+            writer.serialize(record)?;
         }
         Ok(())
-    }
-}
-
-impl From<Vec<TermStem>> for TermStems {
-    fn from(term_stems: Vec<TermStem>) -> Self {
-        let mut entries = BTreeMap::new();
-        for term_stem in term_stems {
-            entries.insert(term_stem.term, term_stem.dpd_stem);
-        }
-        Self { entries }
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn test_from_term_stems() {
-        let term_stems = TermStems::from(vec![TermStem {
-            term: String::from("jumped"),
-            dpd_stem: Some(String::from("jump")),
-        }]);
-
-        assert_eq!(term_stems.entries.len(), 1);
-        assert_eq!(
-            term_stems.entries.get("jumped").unwrap(),
-            &Some(String::from("jump"))
-        );
     }
 }
