1851eeb313a03e68b34f0af98b5f42ab89c1a901
jr <jhanarato@gmail.com>
2026-09-10 10:05:30 +0800

    Extract function analyze_text()

--- src/main.rs
+++ src/main.rs
@@ -28,24 +28,11 @@ use tantivy::tokenizer::{LowerCaser, TextAnalyzer, Token, TokenStream};
 fn main() -> Result<()> {
     let args = Arguments::parse();
     match args.command {
-        Command::StemTable => { create_stem_table(&args.texts, &args.dpd_db, &args.stem_file)?; }
+        Command::StemTable => {
+            create_stem_table(&args.texts, &args.dpd_db, &args.stem_file)?;
+        }
         Command::Analyze { algorithmic, text } => {
-            let mut analyzer = if algorithmic {
-                let stemmer = algo_stemmer::AlgorithmicStemmer {};
-                TextAnalyzer::builder(PaliTokenizer::default())
-                    .filter(LowerCaser)
-                    .filter(stemmer)
-                    .build()
-            } else {
-                let reader = Reader::from_path(args.stem_file)?;
-                let stemmer = DictionaryStemmer::try_from(reader)?;
-                TextAnalyzer::builder(PaliTokenizer::default())
-                    .filter(LowerCaser)
-                    .filter(stemmer)
-                    .build()
-            };
-            let mut token_stream = analyzer.token_stream(text.as_str());
-            token_stream.process(&mut |token: &Token| println!("{0}", token.text));
+            analyze_text(algorithmic, text.as_str(), &args.stem_file)?;
         }
         Command::DpdLookup { term } => {
             let conn = Connection::open(args.dpd_db.as_path())?;
@@ -98,5 +85,25 @@ fn create_stem_table(
     let dictionary = Dictionary::from(conn);
     let term_stems = TermStems::new(vocabulary, &dictionary);
     term_stems.save(stem_file_path)?;
+    Ok(())
+}
+
+fn analyze_text(algorithmic: bool, text: &str, stem_file_path: &Path) -> Result<()> {
+    let mut analyzer = if algorithmic {
+        let stemmer = algo_stemmer::AlgorithmicStemmer {};
+        TextAnalyzer::builder(PaliTokenizer::default())
+            .filter(LowerCaser)
+            .filter(stemmer)
+            .build()
+    } else {
+        let reader = Reader::from_path(stem_file_path)?;
+        let stemmer = DictionaryStemmer::try_from(reader)?;
+        TextAnalyzer::builder(PaliTokenizer::default())
+            .filter(LowerCaser)
+            .filter(stemmer)
+            .build()
+    };
+    let mut token_stream = analyzer.token_stream(text);
+    token_stream.process(&mut |token: &Token| println!("{0}", token.text));
     Ok(())
 }
