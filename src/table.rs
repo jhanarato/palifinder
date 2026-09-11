838596bc207ca842eb676b0ebbfeb538e8f5ee69
jr <jhanarato@gmail.com>
2026-09-11 09:47:38 +0800

    Added impl<T> TryFrom<Reader<T>> for StemTable

--- src/table.rs
+++ src/table.rs
@@ -1,7 +1,7 @@
 use crate::dpd::Dictionary;
 use crate::vocabulary::Vocabulary;
-use anyhow::Result;
-use csv::Writer;
+use anyhow::{Error, Result};
+use csv::{Reader, Writer};
 use serde::{Deserialize, Serialize};
 use std::path::Path;
 use crate::snowball;
@@ -15,7 +15,7 @@ pub struct TermStem {
 
 #[derive(Clone)]
 pub struct StemTable {
-    entries: Vec<TermStem>,
+    records: Vec<TermStem>,
 }
 
 impl StemTable {
@@ -26,7 +26,7 @@ impl StemTable {
             let snowball_stem = snowball::pali_stem(term.as_str());
             entries.push(TermStem { term, dpd_stem, snowball_stem });
         }
-        Self { entries }
+        Self { records: entries }
     }
 
     fn dpd_stem(term: &str, dictionary: &Dictionary) -> Option<String> {
@@ -40,9 +40,41 @@ impl StemTable {
     #[allow(clippy::missing_errors_doc)]
     pub fn save(&self, path: &Path) -> Result<()> {
         let mut writer = Writer::from_path(path)?;
-        for record in &self.entries {
+        for record in &self.records {
             writer.serialize(record)?;
         }
         Ok(())
+    }
+}
+
+impl<T> TryFrom<Reader<T>> for StemTable
+where
+    T: std::io::Read,
+{
+    type Error = Error;
+    fn try_from(mut reader: Reader<T>) -> Result<Self, Self::Error> {
+        let mut records: Vec<TermStem> = Vec::new();
+        for record in reader.deserialize() {
+            records.push(record?);
+        }
+        Ok(Self { records })
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use csv::Reader;
+
+    const STEM_DATA: &str = "\
+term,dpd_stem,snowball_stem
+jumped,jump,jump
+jumping,jump,jump
+frog,,frog";
+
+    #[test]
+    fn test_try_from_reader() {
+        let reader = Reader::from_reader(STEM_DATA.as_bytes());
+        let _table = StemTable::try_from(reader);
     }
 }
