72582a06d301425d4de4354a41eaea2d36ee7048
jr <jhanarato@gmail.com>
2026-09-19 12:38:43 +0800

    Create end to end example with custom stemmer.

--- src/index.rs
+++ src/index.rs
@@ -1,11 +1,13 @@
 use crate::analyzers::AnalyzerConfig;
-use anyhow::Result;
-use tantivy::Index;
 use tantivy::schema::{IndexRecordOption, Schema, TextFieldIndexing, TextOptions};
 use tantivy::tokenizer::TextAnalyzer;
+use tantivy::{Document, Index, IndexWriter, ReloadPolicy, TantivyDocument};
+use tantivy::collector::TopDocs;
+use tantivy::query::QueryParser;
 
-#[allow(clippy::missing_errors_doc)]
-pub fn index() -> Result<Index> {
+#[allow(unused)]
+#[allow(clippy::missing_panics_doc)]
+pub fn create_index_in_ram_with_document() {
     let mut schema_builder = Schema::builder();
 
     let uid_options = TextOptions::default()
@@ -27,9 +29,55 @@ pub fn index() -> Result<Index> {
     schema_builder.add_text_field("contents", contents_options);
     let schema = schema_builder.build();
 
-    let pli_stem = TextAnalyzer::try_from(AnalyzerConfig::Algorithmic)?;
-    let index = Index::builder().schema(schema).create_in_ram()?;
+    let pli_stem = TextAnalyzer::try_from(AnalyzerConfig::Algorithmic).unwrap();
+    let index = Index::builder()
+        .schema(schema.clone())
+        .create_in_ram()
+        .unwrap();
     index.tokenizers().register("pli_stem", pli_stem);
-    
-    Ok(index)
+
+    let mut index_writer: IndexWriter = index.writer(50_000_000).unwrap();
+
+    let uid = schema.get_field("uid").unwrap();
+    let contents = schema.get_field("contents").unwrap();
+
+    let mut document = TantivyDocument::default();
+    document.add_text(uid, "mn1");
+    document.add_text(contents, "Evaṁ me sutaṁ—");
+    document.add_text(
+        contents,
+        "ekaṁ samayaṁ bhagavā ukkaṭṭhāyaṁ viharati subhagavane sālarājamūle. ",
+    );
+
+    index_writer.add_document(document);
+    index_writer.commit();
+
+    let reader = index
+        .reader_builder()
+        .reload_policy(ReloadPolicy::OnCommitWithDelay)
+        .try_into()
+        .unwrap();
+
+    let searcher = reader.searcher();
+
+    let query_parser = QueryParser::for_index(&index, vec![uid, contents]);
+
+    let query = query_parser.parse_query("vihar").unwrap();
+
+    let top_docs = searcher.search(&query, &TopDocs::with_limit(10).order_by_score()).unwrap();
+
+    for (_score, doc_address) in top_docs {
+        let retrieved_doc: TantivyDocument = searcher.doc(doc_address).unwrap();
+        println!("{}", retrieved_doc.to_json(&schema));
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_create_index_in_ram_with_document() {
+        create_index_in_ram_with_document();
+    }
 }
